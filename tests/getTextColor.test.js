0ba55d7fc5ce33c8fcb8dd6e3586da045b4bedfa
TimDunphy <timothy.dunphy@gmail.com>
2020-07-27 18:22:59 +0100

    Updated helper func tests for 100% coverage.

--- tests/getTextColor.test.js
+++ tests/getTextColor.test.js
@@ -1,18 +1,45 @@
+const chroma = require('chroma-js')
+
 const getTextColor = require('../helpers/getTextColor')
+const relativeLuminance = require('../helpers/getLuminance')
 
 describe('Check text color calculations', () => {
     test('should return array with three values for use in an RGB color string', () => {
-        expect(getTextColor('white')).toEqual([0, 0, 0])
-        expect(getTextColor('#fff')).toEqual([0, 0, 0])
-        expect(getTextColor('#ffffff')).toEqual([0, 0, 0])
-        expect(getTextColor('rgb(255, 255, 255)')).toEqual([0, 0, 0])
-        expect(getTextColor('hsl(0, 100%, 100%)')).toEqual([0, 0, 0])
+        expect(
+            getTextColor(relativeLuminance(...chroma('white').rgb()))
+        ).toEqual([0, 0, 0])
+        expect(
+            getTextColor(relativeLuminance(...chroma('#fff').rgb()))
+        ).toEqual([0, 0, 0])
+        expect(
+            getTextColor(relativeLuminance(...chroma('#ffffff').rgb()))
+        ).toEqual([0, 0, 0])
+        expect(
+            getTextColor(
+                relativeLuminance(...chroma('rgb(255, 255, 255)').rgb())
+            )
+        ).toEqual([0, 0, 0])
+        expect(
+            getTextColor(
+                relativeLuminance(...chroma('hsl(0, 100%, 100%)').rgb())
+            )
+        ).toEqual([0, 0, 0])
 
-        expect(getTextColor('black')).toEqual([0, 0, 0])
-        expect(getTextColor('#000')).toEqual([0, 0, 0])
-        expect(getTextColor('#000000')).toEqual([0, 0, 0])
-        expect(getTextColor('rgb(0, 0, 0)')).toEqual([0, 0, 0])
-        expect(getTextColor('hsl(0, 0%, 0%)')).toEqual([0, 0, 0])
+        expect(
+            getTextColor(relativeLuminance(...chroma('black').rgb()))
+        ).toEqual([255, 255, 255])
+        expect(
+            getTextColor(relativeLuminance(...chroma('#000').rgb()))
+        ).toEqual([255, 255, 255])
+        expect(
+            getTextColor(relativeLuminance(...chroma('#000000').rgb()))
+        ).toEqual([255, 255, 255])
+        expect(
+            getTextColor(relativeLuminance(...chroma('rgb(0, 0, 0)').rgb()))
+        ).toEqual([255, 255, 255])
+        expect(
+            getTextColor(relativeLuminance(...chroma('hsl(0, 100%, 0%)').rgb()))
+        ).toEqual([255, 255, 255])
 
         expect(Array.isArray(getTextColor('black'))).toEqual(true)
         expect(getTextColor('black').length).toEqual(3)
